ec7283fb52593199eb65a2d7d63e8b05de3e0d71
Ronan Murphy <ronan.murphy@originmarkets.com>
2019-02-09 01:33:19 +0000

    Updated to have mvp leaderboard with fake data.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UsersComponent } from "./users/users.component";
+
+const routes: Routes = [
+    // {path: "", redirectTo: "/leaderboard", pathMatch: "full"},
+    {path: "", component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
